28485152fb529b642b49673880204336c4968b9c
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-03-13 13:29:54 +0800

    Add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-    return <main className="mx-auto px-6">Hello world again</main>;
+    return (
+        <main className="flex h-full flex-col items-center justify-center">
+            <h1 className="text-3xl font-bold">DRCS Memory Game</h1>
+        </main>
+    );
 }
