8ec272f69ceaba39563df0b0a8c72b7011c046fd
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-03-14 03:16:21 +0800

    feat: Add instructions to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,21 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <main className="flex h-full flex-col items-center justify-center gap-8">
-            <h1 className="text-3xl font-bold">DRCS Memory Game</h1>
-            <p className="max-w-prose text-muted-foreground">
-                Memorize the location of the green squares!
+        <main className="flex h-full flex-col items-center justify-center gap-8 p-4">
+            <h1 className="text-center text-3xl font-bold sm:text-5xl">
+                DRCS Memory Game
+            </h1>
+            <p className="max-w-prose text-balance text-center text-lg text-muted-foreground sm:text-3xl">
+                You will be shown a grid, with a few random green squares.
+                Memorize the locations of the green squares! <br />
             </p>
-            <div className="flex gap-8">
-                <Button asChild>
-                    <Link href="/start">Start Game</Link>
-                </Button>
-                <Button asChild variant="outline">
-                    <Link href="/instructions">Instructions</Link>
+            <p className="max-w-prose text-balance text-center text-lg text-muted-foreground sm:text-3xl">
+                The green squares will be hidden after a few seconds. Click on
+                the locations for all the green squares!
+            </p>
+            <div className="mt-4 flex gap-8">
+                <Button asChild size="xl">
+                    <Link href="/start">Begin!</Link>
                 </Button>
             </div>
         </main>
