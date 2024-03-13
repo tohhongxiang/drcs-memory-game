a050e5f63a407130799b4c93bf12e3c7714afc58
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-03-13 14:18:50 +0800

    Add game board

--- app/start/_components/game.tsx
+++ app/start/_components/game.tsx
@@ -1,7 +1,63 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
 interface GameProps {
     size: number;
 }
 
 export default function Game({ size }: GameProps) {
-    return <div>This is the game board {size}</div>;
+    const [cells, setCells] = useState(
+        [...Array(size * size)].map((_, index) => ({
+            id: index,
+            selected: false,
+        }))
+    );
+
+    const handleClick = (id: number) => {
+        setCells((previousCells) =>
+            previousCells.map((cell) =>
+                cell.id === id ? { ...cell, selected: !cell.selected } : cell
+            )
+        );
+    };
+
+    return (
+        <div
+            style={{
+                gridTemplateColumns: `repeat(${size}, 1fr)`,
+            }}
+            className="grid aspect-square h-full w-full gap-1 md:gap-2"
+        >
+            {cells.map(({ id, selected }) => (
+                <GameCell
+                    key={id}
+                    selected={selected}
+                    onClick={() => handleClick(id)}
+                />
+            ))}
+        </div>
+    );
+}
+
+interface GameCellProps
+    extends React.DetailedHTMLProps<
+        React.ButtonHTMLAttributes<HTMLButtonElement>,
+        HTMLButtonElement
+    > {
+    selected?: boolean;
+}
+
+function GameCell({ selected = false, ...props }: GameCellProps) {
+    return (
+        <button
+            {...props}
+            className={cn(
+                "box-border h-full w-full rounded-md border-4 border-gray-300 border-transparent bg-blue-400 transition duration-75",
+                !selected && "hover:scale-105",
+                selected && "scale-95 border-red-400 hover:scale-100"
+            )}
+        ></button>
+    );
 }
--- app/start/page.tsx
+++ app/start/page.tsx
@@ -3,8 +3,9 @@ import Game from "./_components/game";
 export default function StartGame() {
     return (
         <div className="flex h-full flex-col items-center justify-center">
-            <h1>Game</h1>
-            <Game size={3} />
+            <div className="h-[80vw] w-[80vw] lg:h-[50vw] lg:w-[50vw]">
+                <Game size={6} />
+            </div>
         </div>
     );
 }
