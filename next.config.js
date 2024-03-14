72a47c94211872557c5f05d9e611bcb624edf7a5
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-03-15 01:37:49 +0800

    fix: PWA not working due to invalid manifest

--- next.config.js
+++ next.config.js
@@ -16,6 +16,7 @@ const withPWA = require("next-pwa")({
     disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
     register: true, // Register the PWA service worker
     skipWaiting: true, // Skip waiting for service worker activation
+    buildExcludes: [/middleware-manifest.json$/],
 });
 
 // Export the combined configuration for Next.js with PWA support
